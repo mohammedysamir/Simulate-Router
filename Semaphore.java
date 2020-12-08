a4f1228fa9f61bc49f319bf4b92a4d81f0e54064
Mohammed Yasser <mohammedre4a@gmail.com>
2020-12-09 00:45:56 +0200

    feat : semaphore class by (Mariem)

--- Semaphore.java
+++ Semaphore.java
@@ -1,3 +1,37 @@
+import java.io.IOException;
+import java.util.LinkedList;
+import java.util.Queue;
+
 public class Semaphore {
-    
+
+    int value = 1;
+    Queue<Device> queue = new LinkedList<>();
+
+    Semaphore() {
+        value = 1;
+    }
+
+    public synchronized void waiting(Device D) throws IOException, InterruptedException
+        {
+          value--;
+          if(value<0)
+          {
+            queue.add(D);
+            System.out.println("device "+D.getName()+"-"+D.getType()+" has arrived and waiting");
+            D.LoggingTime("device "+D.getName()+"-"+D.getType()+" has arrived and waiting");
+            wait();
+           }
+         }
+
+    public synchronized void signal(Device D)
+         {
+           value++;
+           if(value<=0)
+           {
+             queue.remove(D);
+             notify();
+           }
+           
+         }
+   
 }
