7dfd640667be8ff1a4bcb5059e12433f3d52bf98
Mohammed Yasser <mohammedre4a@gmail.com>
2020-12-09 01:06:52 +0200

    feat : create class Router by (Mariem)

--- Router.java
+++ Router.java
@@ -1,3 +1,28 @@
+import java.io.IOException;
+import java.util.*;
+
 public class Router {
-    
+    int maxdevice;
+    Semaphore c;
+    ArrayList<Device> devices = new ArrayList<Device>();
+
+    Router(int m) {
+        maxdevice = m;
+    }
+
+    public void connect(Device device) throws IOException, InterruptedException
+ {
+   c.waiting(device);
+   if(devices.size()!=maxdevice){
+     devices.add(device);
+   }
+ }
+ public void disconnect(Device device){
+   for(int i=0;i<devices.size();i++){
+     if(devices.get(i)==device) devices.remove(i);
+   }
+   c.signal(device);
+ }
+
+
 }
