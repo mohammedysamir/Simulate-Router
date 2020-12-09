f91ac615320dde9faf9fc176602e8c61213ccda5
Mohammed Yasser <mohammedre4a@gmail.com>
2020-12-09 02:09:28 +0200

    refactor: add String object to store output data

--- Writer.java
+++ Writer.java
@@ -3,9 +3,11 @@ import java.io.IOException;
 //class to write log text into Log file
 public class Writer {
     static String log = "Log.txt";
+    static String out="";
     static void Write(String text) throws IOException {
-        FileWriter output = new FileWriter(log);
-        output.write(text);
+        out+=text+System.lineSeparator();
+        FileWriter output=new FileWriter(log);
+        output.write(out);
         output.close();
     }
 }
