a44fb19daf3a4e8a8a7f3d3a9c15daeb62dafdba
Mohammed Yasser <mohammedre4a@gmail.com>
2020-12-10 15:02:46 +0200

    refactor : adding parameterized constructor to init value.

--- Semaphore.java
+++ Semaphore.java
@@ -4,39 +4,37 @@ import java.util.Queue;
 
 public class Semaphore {
 
-    int value = 0;
-    Queue<Device> queue = new LinkedList<>();
+  int value = 1;
+  Queue<Device> queue = new LinkedList<>();
 
-    Semaphore() {
-        value = 0;
+  Semaphore() {
+    value = 1;
+  }
+
+  Semaphore(int v) {
+    value = v;
+  }
+
+  public synchronized void waiting(Device D) throws IOException, InterruptedException {
+    value--;
+    if (value < 0) {
+      queue.add(D);
+      System.out.println("device " + D.getname() + "-" + D.getType() + " has arrived and waiting");
+      D.LoggingTime("device " + D.getname() + "-" + D.getType() + " has arrived and waiting");
+      wait();
+    } else {
+      System.out.println("device " + D.getname() + "-" + D.getType() + " has arrived");
+      D.LoggingTime("device " + D.getname() + "-" + D.getType() + " has arrived");
+    }
+  }
+
+  public synchronized void signal(Device D) {
+    value++;
+    if (value <= 0) {
+      queue.remove(D);
+      notify();
     }
 
-    Semaphore(int v){value=v;}
-    public synchronized void waiting(Device D) throws IOException, InterruptedException
-        {
-          value--;
-          if(value<0)
-          {
-            queue.add(D);
-            System.out.println("device "+D.getname()+"-"+D.getType()+" has arrived and waiting");
-            D.LoggingTime("device "+D.getname()+"-"+D.getType()+" has arrived and waiting");
-            wait();
-           }
-           else{
-            System.out.println("device "+D.getname()+"-"+D.getType()+" has arrived");
-            D.LoggingTime("device "+D.getname()+"-"+D.getType()+" has arrived");
-           }
-         }
+  }
 
-    public synchronized void signal(Device D)
-         {
-           value++;
-           if(value<=0)
-           {
-             queue.remove(D);
-             notify();
-           }
-           
-         }
-   
 }
