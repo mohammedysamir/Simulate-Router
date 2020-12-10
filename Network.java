573dc6e19df005f6f8522964af8241ba1855be27
Mohammed Yasser <mohammedre4a@gmail.com>
2020-12-10 14:55:13 +0200

    feat : Take input : number of devices , max number of dev , array of devices and calling r.run.

--- Network.java
+++ Network.java
@@ -1,13 +1,32 @@
+import java.io.IOException;
 import java.util.*;
+
 public class Network {
-    static void main(String[] args){
-        Scanner scan=new Scanner(System.in);
-        System.out.println("Welcome to Routersim");
-        int NumberofUsers,MaxNumber;
+    public static void main(String[] args) throws IOException, InterruptedException {
+        Scanner scan = new Scanner(System.in);
+        String input;
+        ArrayList<Device> device = new ArrayList<Device>();
+        System.out.println("*Welcome to RouterSim*");
+
+        int NumberofUsers, MaxNumber;
+
         System.out.println("Enter Maximum number of allowed devices for router");
-        MaxNumber=scan.nextInt();
+        MaxNumber = scan.nextInt();
+
         System.out.println("Enter Number of devices to connect");
-        NumberofUsers=scan.nextInt();
-        
+        NumberofUsers = scan.nextInt();
+        scan.nextLine();
+        Router r = new Router(MaxNumber);
+
+        for (int i = 0; i < NumberofUsers; i++) {
+            System.out.print("Enter device name :");
+            input = scan.nextLine();
+            Device D = new Device(input, r);
+            device.add(D);
+        }
+
+        r.SetArray(device);
+        r.run();
+        scan.close();
     }
 }
